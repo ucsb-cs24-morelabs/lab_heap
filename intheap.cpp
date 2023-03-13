e222563da3f2a6017e7807b2bde676d7f4c7a180
Unics Yu <yulele28@126.com>
2023-03-13 12:10:44 -0700

    [feat] starter code

---
+++ intheap.cpp
@@ -0,0 +1,54 @@
+// heap.cpp
+// Tianle Yu
+
+#include "heap.h"
+#include <iostream>
+using std::cout;
+
+// ------ Implement these functions ------
+
+// Takes in a vector, and turns it into a heap IN PLACE
+std::vector<int> heapify(std::vector<int> &input)
+{
+}
+
+// Pushes a value into the heap, then ensures
+// the heap is correctly arranged
+void Heap::push(int value)
+{
+}
+
+// Pops the minimum value off the heap
+// (but does not return it), then ensures
+// the heap is correctly arranged
+void Heap::pop()
+{
+}
+
+// Returns the index of the parent of a node at given index (-1 for root)
+int parent(int index)
+{
+}
+
+// Returns the index of the left child of a node at given index (-1 for node that does not exist)
+int leftChild(int index)
+{
+}
+
+// Returns the index of the right child of a node at given index (-1 for node that does not exist)
+int rightChild(int index)
+{
+}
+
+// ------ Funtions below this line are already implemented correctly ------
+// Returns the minimum element in the heap
+int Heap::top()
+{
+    return vdata.at(0);
+}
+
+// Returns true if the heap is empty, false otherwise
+bool Heap::empty()
+{
+    return vdata.empty();
+}
---
+++ intheap.h
@@ -0,0 +1,28 @@
+// heap.h
+// Binary heap tree header file for CS 24 lab
+// Diba Mirza
+
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <iostream>
+#include <vector>
+
+class Heap
+{
+
+public:
+    void push(int value); // insert value; //duplicates are allowed
+    void pop();           // delete the min element
+    int top();
+    bool empty();
+    std::vector<int> heapify(std::vector<int> &input);
+    int parent(int index);
+    int leftChild(int index);
+    int rightChild(int index);
+
+private:
+    std::vector<int> vdata; // store the binary heap tree as a dynamic array
+};
+
+#endif
